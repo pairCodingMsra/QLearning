aadb3cf2c84fd60a2f16766b073d30e4c30865f7
hzphzp <huangzp1104@gmail.com>
2019-09-12 14:21:08 +0800

    fix the bug of new_user

--- api.py
+++ api.py
@@ -2,11 +2,10 @@ import requests
 
 root_url = "https://goldennumber.aiedu.msra.cn"
 
+
 def new_user(nick_name: str) -> bool:
     api_url = "/api/NewUser"
-    parameter = {}
-    parameter["nickName"] = nick_name
+    parameter = {"nickName": nick_name}
     response = requests.get(root_url + api_url, params=parameter)
     status_code = response.status_code
-    return 200 <= response < 300
-
+    return 200 <= status_code < 300
--- test/test_new_user.py
+++ test/test_new_user.py
@@ -5,6 +5,5 @@ import api
 class TestNew_user(TestCase):
 
     def test_new_user(self):
-        api.new_user("huangzp")
-
-        # self.fail()
+        if not api.new_user("huangzp"):
+            self.fail()
